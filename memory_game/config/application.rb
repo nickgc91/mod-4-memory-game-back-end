18e31cdc2dc1f433c6363d1264b32f0d7856b6b5
github username <github email address>
2019-09-26 15:33:45 +0100

    added rack-cors and jwt

--- memory_game/config/application.rb
+++ memory_game/config/application.rb
@@ -9,7 +9,10 @@ Bundler.require(*Rails.groups)
 module MemoryGame
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+          origins '*'
+          resource '*', headers: :any, methods: [:get, :post]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,3 +20,4 @@ module MemoryGame
     # the framework and any gems in your application.
   end
 end
+
