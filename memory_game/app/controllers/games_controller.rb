13747c8724f757748d7497d13aff5cf2353e122f
github username <github email address>
2019-09-26 15:56:30 +0100

    created controllers

---
+++ memory_game/app/controllers/games_controller.rb
@@ -0,0 +1,14 @@
+class GamesController < ApplicationController
+
+    def new_game 
+        game = Game.create(game_params, runtime: 0)
+        render: json 
+    end 
+
+private 
+
+def game_params 
+    params.require(:game).permit(:user_id)
+end 
+
+end
---
+++ memory_game/app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+
+    def signin
+    end 
+    
+end
